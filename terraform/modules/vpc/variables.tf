a70ce8c3d656edc92c24aef66cccc83842407bdf
Luc Mally <lucmally@yahoo.com>
2026-02-26 23:43:20 -0600

    vpc module

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,25 @@
+variable "name" {
+  description = "Project name for resource naming"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "common_tags" {
+  description = "Common tags for all resources"
+  type        = map(string)
+  default     = {}
+}
